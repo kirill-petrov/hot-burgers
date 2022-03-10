1b6340d3e40d0fa276e32a3ebaeefca0307fb1ed
kirill petrov <fantaskiras@gmail.com>
2022-03-10 13:58:46 +0300

    6 done

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,11 +2,17 @@ import React from 'react';
 import restourants from '../sample-restaurants';
 
 class Landing extends React.Component {
+  state = { display: false, title: '', url: '' };
+  displayList= ()=> {
+    console.log(this);
+  }
   render() {
     return (
       <div className='restaurant_select'>
         <div className='restaurant_select_top'>
-          <div className='restaurant_select_top-header font-effect-outline'>Выбери ресторан</div>
+          <div onClick={this.displayList} className='restaurant_select_top-header font-effect-outline'>
+            Выбери ресторан
+          </div>
 
           <div className='arrow_picker'>
             <div className='arrow_picker-up'></div>
