c2ab92465d237e96203a694ce7d9d787b38071b1
kirill petrov <fantaskiras@gmail.com>
2022-03-11 09:05:47 +0300

    10

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,29 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <div className="wrap">
-          <div className="header-content">
+const Header = (props) => (
+  <header className="top">
+    <div className="wrap">
+      <div className="header-content">
 
-            <div className="header-rating">
-              <div className="header-rating_tag">Рейтинг</div>
-              <div className="header-rating_icon">⭐ ⭐ ⭐ ⭐ ⭐</div>
-            </div>
-
-            <div className="header-divider"></div>
-            <h1 className="font-effect-fire-animation">{this.props.title}</h1>
-            <h3>
-              <span>Быстрая доставка</span>
-              <span className="sub-header"> #бургеров</span>
-            </h3>
+        <div className="header-rating">
+          <div className="header-rating_tag">Рейтинг</div>
+          <div className="header-rating_icon">
+            <span
+              role="img"
+              aria-label="Rating: 5 out of 5 stars">⭐ ⭐ ⭐ ⭐ ⭐</span>
           </div>
         </div>
-      </header>
-    )
-  }
-}
+
+        <div className="header-divider"></div>
+        <h1 className="font-effect-fire-animation">{props.title}</h1>
+        <h3>
+          <span>Быстрая доставка</span>
+          <span className="sub-header"> #бургеров</span>
+        </h3>
+      </div>
+    </div>
+  </header>
+)
+
 
 export default Header
